06c3dffb9f975858bc5c3b2a1ef174c8ebfe637c
SamMartin92 <samm92@live.ie>
2022-06-19 22:43:18 +0000

    registers Client model in admin

--- booking/admin.py
+++ booking/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Booking
+from .models import Booking, Client
 
 # Register your models here.
 admin.site.register(Booking)
+admin.site.register(Client)
+
