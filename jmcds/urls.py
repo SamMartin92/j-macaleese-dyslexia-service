ca8e3125fa48f6cb1821232a205560915a6f952d
SamMartin92 <samm92@live.ie>
2022-06-20 22:33:39 +0000

    adds base, index and allauth templates

--- jmcds/urls.py
+++ jmcds/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from booking.views import home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts', include('allauth.urls')),
+    path('', home_page, name='home_page'),
+    path('accounts/', include('allauth.urls')),
 ]
