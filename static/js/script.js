9af3a5ea02c1c98df467945667ea737a653cb880
SamMartin92 <samm92@live.ie>
2022-07-15 11:42:19 +0000

    finishes wiring up emailjs

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,18 @@
 function sendMail(contactForm) {
-    emailjs.send("service_f794snj", "template_o5n61zc", {
-            "form_name": contactForm.name.value,
-            "message": contactForm.message.value,
-            "form_email": contactForm.emailaddress.value,
-            "form_phone": contactForm.phone.value
-        }, "Hd3kDcWXoTUyVhO8I")
-        .then(
-            function (response) {
-                console.log("Yay", response);
-            }
-        ),
-        function (error) {
-            console.log("Error", error)
-        };
+        emailjs.send("service_f794snj", "template_3as9mrv", {
+            form_name: contactForm.name.value,
+            form_email: contactForm.emailaddress.value,
+            form_phone: contactForm.phone.value,
+            message: contactForm.message.value
+        })
+            .then(
+                function (response) {
+                    console.log("Yay", response);
+                }
+            ),
+            function (error) {
+                console.log("Error", error)
+            };
 
-    
-}
+            return false
+    }
