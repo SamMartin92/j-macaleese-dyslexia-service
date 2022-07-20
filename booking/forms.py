2a1f490aeb2e83b884503651e1a72785fe14d10d
SamMartin92 <samm92@live.ie>
2022-07-20 12:50:41 +0000

    adds accordian to view_bookings and widget to make_bookings form

--- booking/forms.py
+++ booking/forms.py
@@ -1,7 +1,10 @@
 from django import forms
+from django.forms.widgets import NumberInput
 from .models import Booking
 
 class BookingForm(forms.ModelForm):
+
+    booking_date = forms.DateField(widget=NumberInput(attrs={'type': 'date'}))
     class Meta:
         model = Booking
         fields = ('class_type', 'booking_type', 'booking_date',
