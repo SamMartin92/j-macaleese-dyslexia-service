8d173c6bdec1eeb088292b04d9a5438430b56b08
SamMartin92 <samm92@live.ie>
2022-08-07 17:42:46 +0000

    fixes booking modal and console issues in script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,10 @@
+//declare constants
 const contactForm = document.getElementById("contact-form");
-const successMessage = document.getElementById("success-message")
-
-
-const guardianCheckBox = document.getElementById("is_guardian")
-const bookingSubmit = document.getElementById("booking-submit")
-
-
+const successMessage = document.getElementById("success-message");
+const guardianCheckBox = document.getElementById("is_guardian");
+const bookingSubmit = document.getElementById("booking-submit");
+const modalContent = document.querySelector(".modal-content");
+//define functions
 function sendMail(contactForm) {
     emailjs.send("service_f794snj", "template_3as9mrv", {
             form_name: contactForm.name.value,
@@ -27,24 +26,8 @@ function sendMail(contactForm) {
     return false
 }
 
-
-function childsNameDisableToggle() {
-
-
-
-}
-
-guardianCheckBox.addEventListener("click", function () {
-    let childNameInput = document.getElementById("childs_name");
-    childNameInput.toggleAttribute("disabled");
-});
-
-
-bookingSubmit.addEventListener("click", function () {
+function emptyDateModal() {
     let dateInput = document.getElementById("id_booking_date")
-    let modalContent = document.querySelector(".modal-content")
-    
-
     if (dateInput.value == "") {
         modalContent.innerHTML = `<div class="modal-header">
         <h5 class="modal-title" id="staticBackdropLabel">Booking Unsuccessful</h5>
@@ -56,11 +39,11 @@ bookingSubmit.addEventListener("click", function () {
         <div class="modal-footer">
             <button type="button" id="close-modal-btn" class="btn btn-secondary" data-bs-dismiss="modal">Ok!</button>
         </div>`
+    }
+}
 
-        let closeModalBtn = document.getElementById("close-modal-btn")
-
-        closeModalBtn.addEventListener("click", function () {
-            modalContent.innerHTML = `<div class="modal-header">
+function resetModalContent() {
+    modalContent.innerHTML = `<div class="modal-header">
             <h5 class="modal-title" id="staticBackdropLabel">Booking Submitted</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
@@ -75,7 +58,27 @@ bookingSubmit.addEventListener("click", function () {
             <button type="button" id="close-modal-btn" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             <button type="submit" class="btn btn-primary">I understand</button>
         </div>`
-        });
-    }
+}
 
-})
+function resetTimeout() {
+    setTimeout(resetModalContent, 500)
+}
+
+function onEmptyDateModalClose() {
+    let closeModalBtn = document.getElementById("close-modal-btn")
+    closeModalBtn.addEventListener("click", resetTimeout)
+};
+// initiates event listeners if present on page
+if (bookingSubmit != null) {
+    bookingSubmit.addEventListener("click", () => {
+        emptyDateModal();
+        onEmptyDateModalClose();
+    })
+}
+
+if (guardianCheckBox != null) {
+    guardianCheckBox.addEventListener("click", () => {
+        let childNameInput = document.getElementById("childs_name");
+        childNameInput.toggleAttribute("disabled");
+    });
+}
