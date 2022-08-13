3b2fb5c58d9601aafb3e43f56dc612fcadf879de
SamMartin92 <samm92@live.ie>
2022-08-13 20:55:44 +0000

    fixes allowed hosts to try fix server error

--- jmcds/settings.py
+++ jmcds/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://jmcds.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['jmcds.herokuapp.com', 'localhost']
 CSRF_TRUSTED_ORIGINS = ['https://jmcds.herokuapp.com//', 'https://8000-sammartin92-jmacaleesed-01axxq9kfz7.ws-eu60.gitpod.io']
 
 
