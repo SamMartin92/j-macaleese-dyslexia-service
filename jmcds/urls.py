6b5d171397ac15fe70025b4bfaf9f877376552c1
SamMartin92 <samm92@live.ie>
2022-08-15 11:32:48 +0000

    removes blog app

--- jmcds/urls.py
+++ jmcds/urls.py
@@ -28,7 +28,6 @@ urlpatterns = [
     path('edit_booking/<booking_id>', views.edit_booking, name='edit_booking'),
     path('cancel_booking/<booking_id>', views.cancel_booking, 
          name='cancel_booking'),
-    # path('blog/', include('blog.urls'), name='blog'),
     path('accounts/', include('allauth.urls')),
     path('summernote/', include('django_summernote.urls')),
 ]
