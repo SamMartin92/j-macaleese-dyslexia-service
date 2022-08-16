c62daf74a28f4c7479e433ecf625ffc76d6f62c3
SamMartin92 <samm92@live.ie>
2022-08-16 11:11:01 +0000

    debug to false

--- jmcds/settings.py
+++ jmcds/settings.py
@@ -160,6 +160,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+WHITENOISE_MANIFEST_STRICT = False
 
 MEDIA_URL = '/media/'
 # Default primary key field type
